64a6a72036117f8c93695ddf9545c37bedd9a422
Mees-H <meesconfirmed@gmail.com>
2024-03-08 11:10:10 +0100

    init project + express+ morgan logging

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const {setupLogging} = require("./logging");
+
+const app = express()
+const port = 3000;
+
+setupLogging(app);
+
+app.get('/', (req, resp) => {
+    return resp.send('HELLO WORLD!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
